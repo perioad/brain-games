0224b6e92170e721da7f9833689fa192ba179c48
Alexander Perio <45464847+perioad@users.noreply.github.com>
2019-05-24 13:56:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default = () => {
+export default () => {
   const userName = readlineSync.question('May I have your name? ');
   return console.log(`Hello, ${userName}`);
 };
